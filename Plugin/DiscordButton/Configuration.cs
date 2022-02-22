b48ca8e7e535d3ff7a666f30a3c228360a13496f
Efe.md <95052851+Efemd@users.noreply.github.com>
2022-02-22 17:10:37 +0300

    DiscordButton
    Plugin And UI | Source Files

---
+++ Plugin/DiscordButton/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.API;
+
+namespace DiscordButton
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public ushort EffectID;
+        public string Description;
+        public string URL;
+        public void LoadDefaults()
+        {
+            EffectID = 10300;
+            Description = "Description";
+            URL = "URL";
+        }
+    }
+}
---
+++ Plugin/DiscordButton/DiscordButton.cs
@@ -0,0 +1,52 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using UnityEngine;
+
+namespace DiscordButton
+{
+    public class DiscordButton : RocketPlugin<Configuration>
+    {
+        protected override void Load()
+        {
+            U.Events.OnPlayerConnected += Connected;
+            UnturnedPlayerEvents.OnPlayerDeath += Death;
+            UnturnedPlayerEvents.OnPlayerRevive += Revive;
+            EffectManager.onEffectButtonClicked += ButtonClicked;
+        }
+
+        protected override void Unload()
+        {
+            U.Events.OnPlayerConnected -= Connected;
+            UnturnedPlayerEvents.OnPlayerDeath -= Death;
+            UnturnedPlayerEvents.OnPlayerRevive -= Revive;
+            EffectManager.onEffectButtonClicked -= ButtonClicked;
+        }
+
+        private void Revive(UnturnedPlayer player, Vector3 position, byte angle)
+        {
+            EffectManager.sendUIEffect(Configuration.Instance.EffectID, 101, player.SteamPlayer().transportConnection, true);
+        }
+
+        private void Death(UnturnedPlayer player, EDeathCause cause, ELimb limb, CSteamID murderer)
+        {
+            EffectManager.askEffectClearByID(Configuration.Instance.EffectID, player.SteamPlayer().transportConnection);
+        }
+
+        private void ButtonClicked(Player caller, string buttonName)
+        {
+            if (buttonName == "Discord")
+            {
+                caller.sendBrowserRequest(Configuration.Instance.Description, Configuration.Instance.URL);
+            }
+        }
+
+        private void Connected(UnturnedPlayer player)
+        {
+            EffectManager.sendUIEffect(Configuration.Instance.EffectID, 101, player.SteamPlayer().transportConnection, true);
+        }
+    }
+}
